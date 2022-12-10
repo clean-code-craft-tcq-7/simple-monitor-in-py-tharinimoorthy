11ee6dfc4af86b77a7224f24c2ff0ab1a77a7153
tharinimoorthy <tharinimoorthy55@gmail.com>
2022-12-11 03:02:10 +0530

    Add files via upload

---
+++ thresholds.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 10 17:52:29 2022
+
+@author: TRO1COB
+"""
+
+TEMPERATURE_MIN = 0
+TEMPERATURE_MAX = 45
+STATE_OF_CHARGE_MIN = 20
+STATE_OF_CHARGE_MAX = 80
+CHAGE_RATE_MAX = 0.8 
