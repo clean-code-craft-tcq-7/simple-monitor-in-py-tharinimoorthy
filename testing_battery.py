0935621672e129f1ee3ea5aec1495d38be9e44e6
tharinimoorthy <tharinimoorthy55@gmail.com>
2022-12-11 18:29:34 +0530

    Update testing_battery.py

--- testing_battery.py
+++ testing_battery.py
@@ -12,6 +12,8 @@ from check_charge_rate import charge_rate_beyond_limit
 TEMPERATURE_THRESHOLD_TEST_VALUES = {"-1":True, "0":False, "1":False, "45":False, "46":True}
 SOC_THRESHOLD_TEST_VALUES = {"19":True, "20":False, "21":False, "80":False, "81":True}
 CHARGE_STATE_TEST_VALUES = {"0.79":False, "0.8":False, "0.81":True}
+SOC_CHARGE_RATE_TEST_VALUES = {"0":[[19, 0.8]], "1":[[25, 0.8]]} # here charge rate is constant as only one condition is checked in function
+TEMP_SOC_CHARGE_RATE_TEST_VALUES = {"0":[[46, 19, 0.8]], "1":[[25, 25, 0.8]]} # here soc & charge rate is constant as only one condition is checked in function
 
 if __name__ == '__main__':
   assert(battery_is_ok(25, 70, 0.79) is True)
