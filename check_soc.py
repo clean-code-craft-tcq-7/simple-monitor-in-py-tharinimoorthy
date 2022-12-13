8e2214d289e757126a0011dac5503eebcb89ef40
tharinimoorthy <tharinimoorthy55@gmail.com>
2022-12-13 18:02:11 +0530

    Update check_soc.py

--- check_soc.py
+++ check_soc.py
@@ -8,6 +8,5 @@ Created on Sat Dec 10 17:37:53 2022
 import thresholds as bt
 def soc_beyond_limit(soc):
     if soc < bt.STATE_OF_CHARGE_MIN or soc > bt.STATE_OF_CHARGE_MAX:
-        
         return True
     return False
