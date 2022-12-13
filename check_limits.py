f619fa146e19cf0eb3747bbd989eb316d453b685
tharinimoorthy <tharinimoorthy55@gmail.com>
2022-12-13 18:01:16 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -4,10 +4,8 @@ from check_soc import *
 
 
 def battery_is_ok(temperature, soc):
-    battery_condition =False
     battery_temp= temperature_beyond_limit(temperature)
     battery_soc= soc_beyond_limit(soc)
     if (battery_temp and battery_soc):
-        battery_condition = True
-        return battery_condition
-    return battery_condition
+        return False
+    return True
