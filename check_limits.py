74f11160095b5c060705e2496d5c9c057c3d7db7
tharinimoorthy <tharinimoorthy55@gmail.com>
2022-12-13 16:31:21 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -3,14 +3,11 @@ from check_temperature import *
 from check_soc import *
 
 
-def battery_is_ok(temperature, soc):  
-    if temperature < 0 or temperature > 45:
-      print('Temperature is out of range!')
-      return False
-    elif soc < 20 or soc > 80:
-      print('State of Charge is out of range!')
-      return False
-   
-    if temperature_beyond_limit(temperature) and soc_beyond_limit(soc):
-       return False
-    return True
+def battery_is_ok(temperature, soc):
+    battery_condition =False
+    battery_temp= temperature_beyond_limit(temperature)
+    battery_soc= soc_beyond_limit(soc)
+    if (battery_temp and battery_soc):
+        battery_condition = True
+       return battery_condition
+    return battery_condition
