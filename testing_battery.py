028494215e4cb0cb023ae7bf914defcf0eadb7c6
tharinimoorthy <tharinimoorthy55@gmail.com>
2022-12-13 16:13:49 +0530

    Update testing_battery.py

--- testing_battery.py
+++ testing_battery.py
@@ -22,6 +22,13 @@ if __name__ == '__main__':
   assert(battery_is_ok(50, 25, 0) is False)
   assert(battery_is_ok(40, 25, 0) is True)
   assert(battery_is_ok(25, 70, 0.9) is False)
+   assert(battery_is_ok(-1, 50,0.9) is False)
+  assert(battery_is_ok(46, 60,0.9) is False)
+  assert(battery_is_ok(1, 19,0.9) is False)
+  assert(battery_is_ok(28, 81,0.9) is False)
+  assert(battery_is_ok(25, 70,0.9) is True)
+  assert(battery_is_ok(-20, 9,0.9) is False)
+  assert(battery_is_ok(100, 100,0.9) is False)
   
   print("All is Well!")
   
