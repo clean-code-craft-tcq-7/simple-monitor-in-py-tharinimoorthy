712b91307c03850af651d9052b4bf074bb084c5d
tharinimoorthy <tharinimoorthy55@gmail.com>
2022-12-13 18:06:19 +0530

    Update testing_battery.py

--- testing_battery.py
+++ testing_battery.py
@@ -6,13 +6,12 @@ Created on Sat Dec 10 17:53:34 2022
 """
 from check_limits import battery_is_ok
 if __name__ == '__main__':
-  assert(battery_is_ok(-1, 50) is False)
-  assert(battery_is_ok(46, 60) is False)
-  assert(battery_is_ok(1, 19) is False)
+  assert(battery_is_ok(-5, 60) is False)
+  assert(battery_is_ok(46, 80) is False)
+  assert(battery_is_ok(5, 19) is False)
   assert(battery_is_ok(28, 81) is False)
-  assert(battery_is_ok(25, 70) is True)
-  assert(battery_is_ok(-20, 9) is False)
-  assert(battery_is_ok(100, 100) is False)  
+  assert(battery_is_ok(25, 50) is True)
+  assert(battery_is_ok(-20, 9) is False)  
   print("All is Well!")
   
   
